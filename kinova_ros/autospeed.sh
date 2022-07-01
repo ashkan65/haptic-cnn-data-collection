239f25402792f957567d8621d80df2c2f63663f7
Nolan <107893469+c643393711@users.noreply.github.com>
2022-07-01 15:36:29 -0700

    fix bugs

--- kinova_ros/autospeed.sh
+++ kinova_ros/autospeed.sh
@@ -9,13 +9,13 @@ read times
 
 for ((i=1;i<=$times;i++))
 do
-    timeout 2s rostopic pub -r 100 /j2n6s300_driver/in/cartesian_velocity kinova_msgs/PoseVelocity "{twist_linear_x: 0.0, twist_linear_y: 0.0, twist_linear_z: 1.0, twist_angular_x: 0, twist_angular_y: 0.0, twist_angular_z: 0.0}"
+    timeout 2s rostopic pub -r 100 /j2n6s300_driver/in/cartesian_velocity kinova_msgs/PoseVelocity "{twist_linear_x: 0.0, twist_linear_y: 0.0, twist_linear_z: $forward, twist_angular_x: 0, twist_angular_y: 0.0, twist_angular_z: 0.0}"
 
     rosservice call /'j2n6s300_driver'/in/stop
 
     rosservice call /'j2n6s300_driver'/in/start
 
-    timeout 2s rostopic pub -r 100 /j2n6s300_driver/in/cartesian_velocity kinova_msgs/PoseVelocity "{twist_linear_x: 0.0, twist_linear_y: 0.0, twist_linear_z: -1.0, twist_angular_x: 0.0, twist_angular_y: 0.0, twist_angular_z: 0.0}"
+    timeout 2s rostopic pub -r 100 /j2n6s300_driver/in/cartesian_velocity kinova_msgs/PoseVelocity "{twist_linear_x: 0.0, twist_linear_y: 0.0, twist_linear_z: $backward, twist_angular_x: 0.0, twist_angular_y: 0.0, twist_angular_z: 0.0}"
 
     rosservice call /'j2n6s300_driver'/in/stop
 
